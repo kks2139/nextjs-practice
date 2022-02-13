1a60eaddb34f2d1c040cd134bce60189313bb653
kwangsun <eyeyel@naver.com>
2022-02-14 00:33:04 +0900

    sass 플러그인 설치

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,36 @@ import styles from '../styles/Home.module.css'
 
 const Home: NextPage = () => {
   return (
-    <div>NextJS</div>
+    <div className='root'>
+      NextJS
+      <h1 className='title'>Test 입니다.</h1>
+      <ul className='box'>
+        <li>
+          하나
+          <span className='num'>1</span>
+        </li>
+        <li>
+          둘
+          <span className='num'>2</span>
+        </li>
+        <li>
+          셋
+          <span className='num'>3</span>
+        </li>
+      </ul>
+      <style jsx>{`
+        .title {
+          color: blue;
+        }
+        .box {
+          border: 1px solid green;
+          li {
+            margin: 5px 0;
+            background-color: yellow;
+          }
+        }
+      `}</style>
+    </div>
   )
 }
 
