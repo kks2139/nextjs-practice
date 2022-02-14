f5365dfb1ae92a955989ab5aab3662ab2b42c9da
kwangsun <eyeyel@naver.com>
2022-02-15 00:05:39 +0900

    데이터 fetching

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,21 +1,25 @@
 import type { NextPage } from 'next'
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import {Movie, Response} from '../utils/interfaces';
 
 const API_KEY = '450a2d1267e0aaf9403bcd16120a9e62';
 
 const Home: NextPage = () => {
+  const [list, setList] = useState<Movie[]>([]);
+
   useEffect(()=>{
     (async ()=>{
-      const res = await fetch(`/movie/popular?API_KEY=${API_KEY}`);
-      const results: Response<Movie> = res.json();
-
+      const res = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+      const data: Response<Movie> = await res.json();
+      setList(data.results);
     })();
   },[]);
 
   return (
     <div className='home'>
-      HOME !!
+      {list.map(mv => (
+        <div key={mv.id}>{mv.original_title}</div>
+      ))}
       <style jsx>{`
         .home {
 
